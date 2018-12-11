7060076b7d44fc4b72c1f073caf05006bd445a27
sitiannisah11 <siti11annisah@gmail.com>
2018-12-12 06:56:34 +0700

    tugas UAS larapel

---
+++ app/barang.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class barang extends Model
+{
+    //
+    //
+    public function gudang(){
+    	return $this->belongsTo('\App\gudang','gudang_id');
+    }
+}
